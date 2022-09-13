6a4607e8d0712551a4a5ee567cec8c6f35894673
RobertBrugger <robert.brugger@student.allenisd.org>
2022-09-13 14:44:06 -0500

    Has errors but is a start

--- main.swift
+++ main.swift
@@ -1,4 +1,15 @@
-var list: Set = ["cat", "dog", "rat", "mouse", "chicken", "rabbit"]
+var line : [String]?
+repeat {
+   let line = readLine()
+    if line != nil {
+        print("You typed: '\(line!)'")
+    }
+} while line != nil
+
+
+
+var newLine = line
+var list: [String]? = newLine 
 var sortedlist = list.sorted()
 for sortedlist in sortedlist {
     print(sortedlist)
